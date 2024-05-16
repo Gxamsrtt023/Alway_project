da4ae8d69f844a33bcd78078d3b09d0d80f5265a
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-17 05:00:18 +0700

    ตกแต่งเพิ่ม
    สี่เหลี่ยมชมพู

--- src/login.jsx
+++ src/login.jsx
@@ -2,40 +2,43 @@ import React from 'react';
 import './login.css';
 
 function Login() {
-    return (
-        <>
-            <h1 className='login_uptext'>  Login </h1>
+  return (
+    <>
+      <div className="header-rectangle">
+        
+      </div>
 
-            {/* เพิ่มสี่เหลี่ยมผืนผ้าด้านบนของ .telephone */}
-            <div className="telephone">
-                <div className="rectangle"></div>
-                <form>
-                    <label>
-                        Telephone or Email
-                        <input className='login_name'
-                            name='account'
-                            type='text'
-                            autoComplete='off'
-                            maxLength={10}
-                            required
-                        />
-                    </label>
+      <div className="container">
+        <h1 className='login_uptext'>Login</h1>
+        <div className="telephone">
+          <form>
+            <label>
+              Telephone or Email
+              <input className='login_name'
+                name='account'
+                type='text'
+                autoComplete='off'
+                maxLength={10}
+                required
+              />
+            </label>
 
-                    <label className='login_password'>
-                        Password
-                        <input className='login_name'
-                            name='password'
-                            type='password'
-                            autoComplete='off'
-                            required
-                        />
-                    </label>
+            <label className='login_password'>
+              Password
+              <input className='login_name'
+                name='password'
+                type='password'
+                autoComplete='off'
+                required
+              />
+            </label>
 
-                    <button className='login_button'> login </button>
-                </form>
-            </div>
-        </>
-    )
+            <button className='login_button'>Login</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;

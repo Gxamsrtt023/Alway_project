49000f6683cfc107b9e23569ad6bb793bcd4b575
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-22 22:49:36 +0700

    update login

--- src/login.jsx
+++ src/login.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './login.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 function Login() {
   return (
     <>
       <div className="header-rectangle">
+        {/* จะเสนอให้แทรกส่วนหัวของเว็บไซต์ที่นี่ */}
       </div>
 
       <div className="container">
         <div className="box_login">
-          <h1 className='login_uptext'>Login</h1>
+          <h1 className='login_uptext'>
+            Login
+          </h1>
           <form>
             <label>
-              <input className='login_name'
+              
+              <input
+                className='login_name'
                 name='account'
                 type='text'
                 placeholder='Username'
@@ -20,18 +27,21 @@ function Login() {
                 maxLength={10}
                 required
               />
+              <FontAwesomeIcon icon={faUser} />
             </label>
 
             <label className='login_password'>
-              <input className='login_name'
+              <input
+                className='login_name'
                 name='password'
                 type='password'
                 placeholder='Password'
                 autoComplete='off'
                 required
               />
+              <FontAwesomeIcon icon={faLock} />
             </label>
-
+           
             <button className='login_button'>Login</button>
           </form>
         </div>

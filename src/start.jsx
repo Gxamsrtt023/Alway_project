d4b7088a093d4463f295be007d2921958f8618c6
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-24 04:14:04 +0700

    ตกแต่ง

--- src/start.jsx
+++ src/start.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './start.css';
 
+const ALWAY_PROJECT = () => {
+  return (
+    <div className="app" style={{ width: '800px', height: '600px', margin: '0 auto' }}>
+      {/* เนื้อหาของแอป */}
+    </div>
+  );
+};
+
 const Square = ({ color }) => {
   return (
     <div className={`square ${color}`}>
@@ -26,6 +34,11 @@ const App = () => {
       <div className="pink"></div>
       <Square color="green" />
       <StartButton />
+      <img src="/startpic1.png" alt="Decorative" className="decorative-image" />
+      <div className="image-container">
+        <div className="pic2-shadow"></div>
+        <img src="/startpic2.png" alt="Decorative" className="pic2-image" />
+      </div>
     </div>
   );
 };

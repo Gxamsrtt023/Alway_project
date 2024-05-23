bcede6efbc66ac89a3831219033bb4149f84863c
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-23 20:16:09 +0700

    add

---
+++ src/start.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './start.css';
+
+const Square = ({ color }) => {
+  return (
+    <div className={`square ${color}`}>
+      <span></span>
+    </div>
+  );
+};
+
+const StartButton = () => {
+  return (
+    <button className="start-button">
+      START
+    </button>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="pink"></div>
+      <Square color="green" />
+      <StartButton />
+    </div>
+  );
+};
+
+export default App;

3ba2256bbb0a0dde797653f649fb207151e5bfe9
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-27 01:27:57 +0700

    finalQ4

---
+++ src/Q4.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './Q4.css';
+
+function Q4() {
+    return (
+        <div className="_4container">
+            <div className="_4rectangle1"></div>
+            <div className="_4rectangle2"></div>
+            <div className="_4Ask">
+                Question 1/7
+            </div>
+            <div className="_4Asks">
+                เวลาน้ำหนักขึ้นคุณมักจะรู้สึกว่าบริเวณของสะโพกและต้นขาเพิ่มขึ้นมากกว่าส่วนอื่นๆของร่างกายเสมอ
+            </div>
+            <div className="_4line"></div>
+            <div className="_4Number1">
+                1
+            </div>
+            <div className="_4Number2">
+                2
+            </div>
+            <button className="Button4_Ans1">
+                ฉันคิดว่าเป็นเช่นนั้น
+            </button>
+            <button className="Button4_Ans2">
+                ไม่ถึงขนาดนั้น
+            </button>
+        </div>
+    );
+}
+
+export default Q4;

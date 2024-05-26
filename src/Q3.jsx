fa43a9828dd9738a27764749afffff7ece8d7ce8
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-27 00:49:50 +0700

    finalQ3

---
+++ src/Q3.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './Q3.css';
+
+function Q3() {
+    return (
+        <div className="_3container">
+            <div className="_3rectangle1"></div>
+            <div className="_3rectangle2"></div>
+            <div className="_3Ask">
+                Question 1/7
+            </div>
+            <div className="_3Asks">
+                คุณมักจะมีปัญหาในการเลือกซื้อกางเกงเพราะมันมักจะรู้สึกว่าหลวมบริเวณต้นขาและสะโพกใช่หรือไม่
+            </div>
+            <div className="_3line"></div>
+            <div className="_3Number1">
+                1
+            </div>
+            <div className="_3Number2">
+                2
+            </div>
+            <button className="Button3_Ans1">
+                ฉันคิดว่าเป็นเช่นนั้น
+            </button>
+            <button className="Button3_Ans2">
+                ไม่ถึงขนาดนั้น
+            </button>
+        </div>
+    );
+}
+
+export default Q3;

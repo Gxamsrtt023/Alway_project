05c051d110c6faaf7dae3658ba24058e982a6b86
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-27 00:35:04 +0700

    final Q2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './login'
+import Q2 from './Q2'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Login />
+      <Q2 />
     </>
   )
 }
---
+++ src/Q2.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './Q2.css';
+
+function Q2() {
+    return (
+        <div className="_2container">
+            <div className="_2rectangle1"></div>
+            <div className="_2rectangle2"></div>
+            <div className="_2Ask">
+                Question 2/7
+            </div>
+            <div className="_2Asks">
+            คุณคิดว่าเอวของคุณมีขนาดเล็กกว่าสะโพกหรือไม่?
+            </div>
+            <div className="_2line"></div>
+            <div className="_2Number1">
+                1
+            </div>
+            <div className="_2Number2">
+                2
+            </div>
+            <button className="Button2_Ans1">
+                ฉันคิดว่าเป็นเช่นนั้น
+            </button>
+            <button className="Button2_Ans2">
+                ไม่ถึงขนาดนั้น
+            </button>
+        </div>
+    );
+}
+
+export default Q2;

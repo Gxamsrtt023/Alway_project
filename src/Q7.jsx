059048c1c148f1f89e632c1ee558770d684dac21
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-28 00:23:27 +0700

    update

---
+++ src/Q7.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './Q7.css';
+import { FaFemale, FaMale } from 'react-icons/fa';
+
+function Q7() {
+    return (
+        <div className="_7container">
+            <div className="_7rectangle1"></div>
+            <div className="_7rectangle2"></div>
+            <div className="_7Ask">
+                Question 7/7
+            </div>
+            <div className="_7Asks">
+            เลือกสไตล์ที่คุณชื่นชอบเพื่อรับเคล็ดลับในการแต่งตัวในแบบที่คุณต้องการ
+            </div>
+            <div className="_7line"></div>
+            <div className="_7Number1">
+                1
+            </div>
+            <div className="_7Number2">
+                2
+            </div>
+            <button className="Button7_Ans1">
+                เสื้อผ้าสตรี
+            <div className="iconfemale"><FaFemale /> </div>
+            </button>
+            <button className="Button7_Ans2">
+                เสื้อผ้าบุรุษ
+            <div className="iconmale"><FaMale /></div>
+            </button>
+        </div>
+    );
+}
+
+export default Q7;

29149d8a198b2ed31011b645b9b041338dcc8806
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-27 23:57:03 +0700

    update

---
+++ src/Q6.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './Q6.css';
+
+function Q6() {
+    return (
+        <div className="_6container">
+            <div className="_6rectangle1"></div>
+            <div className="_6rectangle2"></div>
+            <div className="_6Ask">
+                Question 6/7
+            </div>
+            <div className="_6Asks">
+            คุณมักจะส่งเสื้อที่ซื้อมาคืนไป  เพราะมันอาจรู้สึกรัดบริเวณไหล่และบริเวณหน้าอก
+            </div>
+            <div className="_6line"></div>
+            <div className="_6Number1">
+                1
+            </div>
+            <div className="_6Number2">
+                2
+            </div>
+            <button className="Button6_Ans1">
+                ฉันคิดว่าเป็นเช่นนั้น
+            </button>
+            <button className="Button6_Ans2">
+                ไม่ถึงขนาดนั้น
+            </button>
+        </div>
+    );
+}
+
+export default Q6;

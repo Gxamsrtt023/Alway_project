4e16cc7719d20e768241f80721e66f346a6bbd98
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-27 23:52:52 +0700

    update Q5

---
+++ src/Q5.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './Q5.css';
+
+function Q5() {
+    return (
+        <div className="_5container">
+            <div className="_5rectangle1"></div>
+            <div className="_5rectangle2"></div>
+            <div className="_5Ask">
+                Question 5/7
+            </div>
+            <div className="_5Asks">
+            คุณมักจะหลีกเลี่ยงการใช้แผ่นรองไหล่เพราะมักอาจจะทำให้คุณรู้สึกเทอะทะ           
+            </div>
+            <div className="_5line"></div>
+            <div className="_5Number1">
+                1
+            </div>
+            <div className="_5Number2">
+                2
+            </div>
+            <button className="Button5_Ans1">
+                ฉันคิดว่าเป็นเช่นนั้น
+            </button>
+            <button className="Button5_Ans2">
+                ไม่ถึงขนาดนั้น
+            </button>
+        </div>
+    );
+}
+
+export default Q5;

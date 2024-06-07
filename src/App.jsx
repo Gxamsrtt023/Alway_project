4be9f59c1a58d76e447407a2e1e183dc1b8fa5cd
ganyawee <111553213+ganyawee@users.noreply.github.com>
2024-06-08 06:51:42 +0700

    update ans1

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './login'
-
+import Answer1 from './answer1'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Login />
+      <Answer1/>
     </>
   )
 }
--- src/answer1.jsx
+++ src/answer1.jsx
@@ -6,7 +6,7 @@ const Answer1 = () => {
         <div className="container">
             <div className="header">
                 <div className="image-wrapper">
-                    <img src="path/to/image" alt="Body shape illustration" className="image"/>
+                    <img src="/นาฬิกาทราย.png" alt="Body shape illustration" className="image"/>
                 </div>
                 <div className="description">
                     <h1>รูปร่างของคุณคือ</h1>
@@ -18,8 +18,8 @@ const Answer1 = () => {
             <div className="section">
                 <h2>เสื้อเชิ้ตทรงมาตรฐาน</h2>
                 <div className="image-row">
-                    <img src="path/to/image1" alt="Standard shirt 1" className="image"/>
-                    <img src="path/to/image2" alt="Standard shirt 2" className="image"/>
+                    <img src="/เสื้อเชิ้ต1.png" alt="Standard shirt 1" className="image"/>
+                    <img src="/เสื้อเชิ้ต2.png" alt="Standard shirt 2" className="image"/>
                 </div>
                 <p>หนึ่งในเครื่องแต่งกายที่ดีที่สุดสำหรับหุ่นนาฬิกาทรายคือเสื้อเชิ้ตพอดีตัวแบบเรียบๆ
                     ไม่เพียงความสบายจะทำให้มั่นใจน้อยของร่างกายคุณคับขันจึงสามารถรักษาลุคตามธรรมชาติได้</p>
@@ -27,8 +27,8 @@ const Answer1 = () => {
             <div className="section">
                 <h2>เดรสสัดรูป</h2>
                 <div className="image-row">
-                    <img src="path/to/image3" alt="Dress 1" className="image"/>
-                    <img src="path/to/image4" alt="Dress 2" className="image"/>
+                    <img src="/เดรสรัดรูป.jpg" alt="Dress 1" className="image"/>
+                    <img src="/เดรสรัดรูป2.png" alt="Dress 2" className="image"/>
                 </div>
                 <p>เดรสสัดรปที่เน้นรูปร่างตามธรรมชาติของคุณนั้นช่วยเสริมส่วนโค้งส่วนเว้าของคุณ
                     และทำให้รูปร่างของคุณสวยยาวขึ้น โดยเน้นรูปร่างนาฬิกาทรายของคุณ</p>

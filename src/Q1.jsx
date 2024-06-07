e16300f25dbb3ab334920fa4e10bb98d3232bbd0
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-06-07 16:55:46 +0700

    new

--- src/Q1.jsx
+++ src/Q1.jsx
@@ -7,10 +7,10 @@ function Q1() {
             <div className="rectangle1"></div>
             <div className="rectangle2"></div>
             <div className="_1Ask">
-                Question 1/7
+                Question 1/1
             </div>
             <div className="_1Asks">
-                คุณคิดว่ารูปร่างของคุณจัดอยู่ในกลุ่มไหน?? 
+                คุณคิดว่ารูปร่างของคุณจัดอยู่ในกลุ่มลักษณะรูปร่างแบบไหน?? 
             </div>
             <div className="line"></div>
             <div className="Number1">
@@ -26,16 +26,16 @@ function Q1() {
                 4
             </div>
             <button className="Button1_Ans1">
-               
+               เอวบาง ไหล่กว้าง
             </button>
             <button className="Button1_Ans3">
-               
+            เอวบาง ไหล่กว้าง
             </button>
             <button className="Button1_Ans2">
-               
+            เอวบาง ไหล่กว้าง
             </button>
             <button className="Button1_Ans4">
-                
+            เอวบาง ไหล่กว้าง
             </button>
             <>
             <img src="/IMG1.png" alt="Decorative" className="i1_mage" />

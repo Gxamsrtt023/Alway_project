01fffb4e1f243864fed1848513d5f65842a1b56c
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-06-08 20:45:56 +0700

    updaate

--- src/answer1.jsx
+++ src/answer1.jsx
@@ -4,6 +4,7 @@ import './answer1.css';
 const Answer1 = () => {
     return (
         <div className="container_ans1">
+             <img src="/บ้านans1.png" alt="Body shape illustration" className="homeans1"/>
             <div className="container11_ans1">
             <div className="rectangle1_ans1"></div>
             <div className="rectangle2_ans1"></div>
@@ -14,11 +15,14 @@ const Answer1 = () => {
                 <div className="description1_ans1">
                     <h1>รูปร่างของคุณคือ</h1>
                     <h2>รูปร่างนาฬิกาทราย</h2>
+                    <img src="/รูปทรงนาฬิกาทราย.png" alt="Standard shirt 1" className="IGMAns1"/>
+                    <div class="line_ans1"></div>
                     <p>คุณมีเอวคอดชัดเจน มีส่วนเว้าส่วนโค้งที่สวยงาม รูปร่างของคุณมีความสมดุลรับกันดี
                         เคล็ดลับการแต่งตัวของเราจะช่วยให้คุณเน้นจุดสวยละมุนไป</p>
                 </div>
             </div>
             </div>
+            <div class="line_ans1_1"></div>
             <div className="container2_ans1">
             <div className="section">
                 <h2>เสื้อเชิ้ตทรงมาตรฐาน</h2>
@@ -35,7 +39,7 @@ const Answer1 = () => {
                     <img src="/เดรสรัดรูป.jpg" alt="Dress 1" className="image"/>
                     <img src="/เดรสรัดรูป2.png" alt="Dress 2" className="image"/>
                 </div>
-                <p>เดรสรัดดรปที่เน้นรูปร่างตามธรรมชาติของคุณนั้นช่วยเสริมส่วนโค้งส่วนเว้าของคุณ
+                <p>เดรสรัดรูปที่เน้นรูปร่างตามธรรมชาติของคุณนั้นช่วยเสริมส่วนโค้งส่วนเว้าของคุณ
                     และทำให้รูปร่างของคุณสวยยาวขึ้น โดยเน้นรูปร่างนาฬิกาทรายของคุณ</p>
             </div>
             </div>

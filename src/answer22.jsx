fff70ec611049cf31d27886c6d688e48b9be2ca5
ganyawee <111553213+ganyawee@users.noreply.github.com>
2024-06-09 21:56:24 +0700

    upp  ans2 ag

---
+++ src/answer22.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './answer2.css';
+
+const Answer2 = () => {
+    return (
+        <div className="container_ANS2">
+             <img src="/บ้านans2.png" alt="Body shape illustration" className="homeANS2"/>
+            <div className="container22_ANS2">
+            <div className="rectangle1_answer2"></div>
+            <div className="rectangle2_answer2"></div>
+            <div className="headerANS2">
+                <div className="image-wrapper-ANSWER2">
+                    <img src="/สามเหลี่ยมคว่ำ.png" alt="Body shape illustration" className="img22"/>
+                </div>
+                <div className="description2_ANS2">
+                    <h1>รูปร่างของคุณคือ</h1>
+                    <h2>รูปร่างสามเหลี่ยมคว่ำ</h2>
+                    <img src="/รูปร่างสามเหลี่ยมคว่ำ.png" alt="Standard shirt 1" className="IMGAns2"/>
+                    <div class="line_ANS2"></div>
+                    <p>คุณมีไหล่ที่สวยงามและร่างกายที่ดูแข็งแรงมั่นคง เคล็ดลับการแต่งตัวของเรา
+                        จะช่วยให้คุณดูมีเสน่ห์ยิ่งขึ้น มาสร้างความสมดุลให้ร่างกาย</p>
+                </div>
+            </div>
+            </div>
+            <div class="line_ANS2_2"></div>
+            <div className="container2_ANS2">
+            <div className="section_ANSwer2 ">
+                <h2>เสื้อปาดไหล่</h2>
+                <div className="imageANSWER2-row">
+                    <img src="/เสื้อปาดไหล่1.jpg" alt="Standard shirt 1" className="imgANS2"/>
+                    <img src="/เสื้อปาดไหล่2.jpg" alt="Standard shirt 2" className="imgANS2"/>
+                </div>
+                <p>เสื้อปาดไหล่จะโชว์ไหล่ของคุณและดึงดูดความสนใจไปที่ใบหน้าที่สวยงามของคุณ 
+                    นอกจากนี้ยังช่วยยืดลำตัวของคุณให้ยาวขึ้น ทำให้เกิดรูปทรงที่สง่างาม</p>
+            </div>
+            <div className="section_ANSwer2 ">
+                <h2>เดรสเกาะอก</h2>
+                <div className="imageANSWER2-row">
+                    <img src="/เดรสเกาะอก.jpg" alt="Dress 1" className="imgANS2"/>
+                    <img src="/เดรสเกาะอก2.jpg" alt="Dress 2" className="imgANS2"/>
+                </div>
+                <p>เดรสเกาะอกสามารถอกโชว์ไหล่สวยได้ในขณะที่เดรสแบบมีปีก
+                    จะช่วยเพิ่มความคมชัดให้กับช่วงเอวและเพิ่มขนาดให้กับสะโพกของคุณ</p>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Answer2;

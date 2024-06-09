a02a0e6826e77af23073176ac593e5c1b3bd92e3
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-06-09 09:47:32 +0700

    Final ans 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Start from './start'
 import Login from './login'
 import Q1 from './Q1'
 import { Route, Routes,BrowserRouter } from 'react-router-dom'
+import Answer4 from './answer4'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/start" element={<Start />}/>
         <Route path="/Q1" element={<Q1 />}/>
+        <Route path="/answer4" element={<Answer4 />}/>
         
     </Routes>
     </BrowserRouter>
